33e5b078d8f42a9dbc59807e00039f3afc2373b4
Oscar <odgomezlopez@gmail.com>
2024-04-22 16:19:40 +0200

    Miniarreglos

---
+++ Assets/Managers/_Utils/UI/PressKey/scripts/PlataformChangeEventer.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.DualShock;
+using UnityEngine.InputSystem.Switch;
+using UnityEngine.InputSystem.XInput;
+using UnityEngine.Localization;
+
+[ExecuteAlways]
+public class PlataformChangeEventer : MonoBehaviour
+{
+    PlayerInput playerInput;
+    [SerializeField] string activeControlScheme="";
+
+    private void Start()
+    {
+        playerInput = FindObjectOfType<PlayerInput>();
+        OnDeviceChange();
+    }
+
+    private void Update()
+    {
+        if (Time.frameCount % 10 == 0)
+        {
+            if (activeControlScheme != playerInput.currentControlScheme)
+            {
+                activeControlScheme = playerInput.currentControlScheme;
+                OnDeviceChange();
+            }
+        }
+    }
+
+    private void OnDeviceChange()
+    {
+        string device = GetDeviceType();
+
+        switch (device)
+        {
+            case "keyboard&mouse":
+                break;
+            case "gamepad":
+                break;
+            case "playstation":
+                break;
+            case "xbox":
+                break;
+            case "switch":
+                break;
+            default : 
+                break;
+        }
+    }
+
+
+    private string GetDeviceType()
+    {
+        string deviceType = "none";
+        string controlScheme = playerInput.currentControlScheme.ToLower();
+
+        if (controlScheme == "keyboard&mouse")
+            deviceType = "keyboard&mouse";
+        else if (controlScheme == "gamepad")
+        {
+            if (Gamepad.current != null)
+            {
+                deviceType = Gamepad.current switch
+                {
+                    DualShockGamepad => "playstation",
+                    XInputController => "xbox",
+                    SwitchProControllerHID => "switch",
+                    _ => "gamepad" // Generic gamepad if type is unknown
+                };
+            }
+        }
+        //TODO Añadir else if para XR, etc. 
+        return deviceType;
+    }
+}
--- Assets/Scripts/GamePlay/ParallaxEffect.cs
+++ Assets/Scripts/GamePlay/ParallaxEffect.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Paral : MonoBehaviour
+public class ParallaxEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float parallaxEffectMultiplier = 0.5f;
+    private float startPosition;
+    private float length;
+
     void Start()
     {
-        
+        startPosition = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        var cam = Camera.main;
+        float temp = (cam.transform.position.x * (1 - parallaxEffectMultiplier));
+        float distance = (cam.transform.position.x * parallaxEffectMultiplier);
+
+        transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
+
+        if (temp > startPosition + length) startPosition += length;
+        else if (temp < startPosition - length) startPosition -= length;
     }
 }
